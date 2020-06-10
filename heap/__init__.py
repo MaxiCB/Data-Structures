ad3e2487a95e1c0c91f579a8212d92a2e8eb73bc
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-10 11:12:40 -0500

    ADD Initial Files
    - Added initial files needed for project

---
+++ heap/__init__.py
@@ -0,0 +1,45 @@
+class Heap:
+    def __init__(self, comparator):
+        self.storage = []
+        self.comparator = comparator
+
+    def insert(self, value):
+        pass
+
+    def delete(self):
+        pass
+
+    def get_priority(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def _bubble_up(self, index):
+        pass
+
+    def _sift_down(self, index):
+        pass
+
+
+class MaxHeap:
+    def __init__(self):
+        self.storage = []
+
+    def insert(self, value):
+        pass
+
+    def delete(self):
+        pass
+
+    def get_max(self):
+        pass
+
+    def get_size(self):
+        pass
+
+    def _bubble_up(self, index):
+        pass
+
+    def _sift_down(self, index):
+        pass
